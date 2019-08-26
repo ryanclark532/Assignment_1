b07ae9b5d2f526a1cca17c9f451013116fbd07b8
ryanclark532 <42229549+ryanclark532@users.noreply.github.com>
2019-08-26 21:04:26 +1000

    Created a system to dynamically update position of car based on the current speed

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,10 @@
 public class Car {
     String type;
-    int currentSpeed;
+    double currentSpeed;
     double speedModifier;
-    int x,y;
+    double x,y;
 
-    public  Car(String type, int currentSpeed, int x, int y) {
+    public  Car(String type, double currentSpeed, int x, int y) {
         this.type = type;
         this.currentSpeed = currentSpeed;
         this.x = x;
@@ -15,18 +15,20 @@ public class Car {
             this.currentSpeed+=1;
         }
     }
-    public void slowDown(){
+    public void slowDown(double currentSpeed){
+        currentSpeed*=0.25;
         while (this.currentSpeed>0){
-            this.currentSpeed-=1;
+            this.currentSpeed-=currentSpeed;
         }
     }
-    public void updatePositon(Road road, int currentSpeed){
-
-        if(road.orientation=="vertical"){
-            this.y+=1;
+    public void updatePositon(Road road, double currentSpeed){
+        double modifiedSpeed;
+        modifiedSpeed=currentSpeed/60;
+        if(road.orientation.equals("vertical")){
+            this.y+=1*modifiedSpeed;
         }
         else {
-            this.x+=1;
+            this.x+=1*modifiedSpeed;
         }
 
     }
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ public class Main {
         Road road=new Road(30,1,1,"horizontal");
         int spaceleft=road.length;
         Car car=new Car("Car",60,1,1);
-        while (spaceleft>1){
-            car.updatePositon(road);
+        while (spaceleft>=0){
+            car.updatePositon(road,car.currentSpeed);
             System.out.println(car.x+" "+car.y);
-            if(spaceleft<=10){
+            if(spaceleft<10){
                 car.currentSpeed-=6;
             }
-            System.out.println(car.currentSpeed);
+            System.out.println(car.currentSpeed+" "+spaceleft);
             spaceleft-=1;
         }
 
